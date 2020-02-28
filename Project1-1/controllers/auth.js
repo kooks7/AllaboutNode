a253b59a55d61aa78f0a5487b7e1fe2c61e43e9a
kooks7 <busanminjae@naver.com>
2020-02-28 22:04:58 +0900

    Add login by using cookie(test)

--- Project1-1/controllers/auth.js
+++ Project1-1/controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get('Cookie').split('=')[1]);
+  const isLoggedIn = req.get('Cookie').split('=')[1];
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
 };
