10dd8195d584944f8606c00095769280e1f7c3c9
kooks7 <busanminjae@naver.com>
2020-03-10 21:03:24 +0900

    Add Password Matching Check

--- Project1-2/routes/auth.js
+++ Project1-2/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // JavaScript에서 제공하는 Destructuring 을 사용하여 check 함수를 가져오자
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -15,9 +15,34 @@ router.post('/login', authController.postLogin);
 // 라우터에는 넣고싶은 만큼 라우터를 추가할 수 있다. 따라서 check() 를 넣어주고 인자로 form 을 넣어주자
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please Enter a Valid Email'),
+
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please Enter a Valid Email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This Email Address if forbidden.');
+        }
+        // default case
+        return true;
+      }),
+    //body(인자1, 인자2) => req.body로 날라오는 값을 검증
+    // 인자1 : 검증할 값, 인자2: default 에러 메세지
+    body(
+      'password',
+      'Please Enter a Password wiht only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      // 알파벳만 가능
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    })
+  ],
   authController.postSignup
 );
 
