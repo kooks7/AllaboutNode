f4c27d9c9c17091a0ee71d882d4e84b26e3dec07
kooks7 <busanminjae@naver.com>
2020-03-10 16:32:12 +0900

    Add Complete Authentication

--- Project1-2/controllers/admin.js
+++ Project1-2/controllers/admin.js
@@ -66,21 +66,24 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
+      return product.save().then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+      });
     })
     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  // app.js에서 가져온 user data
+  Product.find({ userId: req.user._id })
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
@@ -96,7 +99,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  // product id와 userId가 모두 같아야 삭제 가능
+  Product.deleteOne({ _id: prodId, userId: req.user_id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
