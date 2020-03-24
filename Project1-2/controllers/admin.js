1f9273e2942d97c2fe74b06741a172a36b890670
kooks7 <busanminjae@naver.com>
2020-03-24 17:25:08 +0900

    Add Asynchronous request

--- Project1-2/controllers/admin.js
+++ Project1-2/controllers/admin.js
@@ -180,8 +180,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if (!product) {
@@ -193,11 +193,9 @@ exports.postDeleteProduct = (req, res, next) => {
     .then(result => {
       console.log(result);
       console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success!' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Error' });
     });
 };
