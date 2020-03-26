a274119a17a71c5f0f7c6775a812be59c7e2727f
kooks7 <busanminjae@naver.com>
2020-03-26 21:04:12 +0900

    Add First API Server Setting

---
+++ Project1-3/controllers/feed.js
@@ -0,0 +1,16 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: 'First Posts', content: 'This is the first post!' }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  // status : 201 새로운 리소스 생성을 성공했다.
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
