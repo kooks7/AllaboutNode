52931ac07cd743cd4fc87d74a59d2bd20fcd38e5
kooks7 <busanminjae@naver.com>
2020-03-27 17:29:49 +0900

    Add Connection to MongoDB by Using mongoos

--- Project1-3/app.js
+++ Project1-3/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb://localhost:27017/test';
+
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -22,4 +25,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- Project1-3/controllers/feed.js
+++ Project1-3/controllers/feed.js
@@ -1,17 +1,49 @@
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Posts', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: '1',
+        title: 'First Posts',
+        content: 'This is the first post!',
+        imageUrl: 'images/home4.jpg',
+        creator: {
+          name: 'kooks7'
+        },
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: '올바른 값을 입력해주세요', errors: errors.array() });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title);
-  // Create post in db
-  // status : 201 새로운 리소스 생성을 성공했다.
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/home4.jpg',
+    creator: { name: 'kooks7' }
   });
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
