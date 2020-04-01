2faadd6582579c5d42caa1245452c11d3d936a33
kooks7 <busanminjae@naver.com>
2020-04-01 16:51:16 +0900

    Change then block to async/await

--- Project1-3/controllers/auth.js
+++ Project1-3/controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
@@ -12,68 +12,59 @@ exports.signup = (req, res, next) => {
     error.data = errors.array();
     throw error;
   }
-  const email = req.body.email;
-  const name = req.body.name;
-  const password = req.body.password;
-  bcrypt
-    .hash(password, 12)
-    .then(hashedPw => {
-      const user = new User({
-        email: email,
-        password: hashedPw,
-        name: name
-      });
-      return user.save();
-    })
-    .then(result => {
-      res
-        .status(201)
-        .json({ message: '유저가 생성되었습니다.', userId: result._id });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name
     });
+    const result = await user.save();
+    res
+      .status(201)
+      .json({ message: '유저가 생성되었습니다.', userId: result._id });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  let loadedUser;
-  User.findOne({ email: email })
-    .then(user => {
-      if (!user) {
-        const error = new Error('이메일이 존재하지 않아요!');
-        error.statusCode = 401;
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then(isEqual => {
-      if (!isEqual) {
-        const error = new Error('잘못된 password 에요');
-        error.statusCode = 401;
-        throw error;
-      }
-      const token = jwt.sign(
-        {
-          email: loadedUser.email,
-          userId: loadedUser._id.toString()
-        },
-        'somesupersupersecretfromminjae',
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error('이메일이 존재하지 않아요!');
+      error.statusCode = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('잘못된 password 에요');
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString()
+      },
+      'somesupersupersecretfromminjae',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token: token, userId: user._id.toString() });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.getUserStatus = (req, res, next) => {
@@ -94,22 +85,22 @@ exports.getUserStatus = (req, res, next) => {
     });
 };
 
-exports.updateStatus = (req, res, next) => {
-  const userId = req.userId;
-  const status = req.body.status;
-  User.findById(userId)
-    .then(user => {
-      user.status = status;
-      return user.save();
-    })
-    .then(result => {
-      console.log(result);
-      res.status(201).json({ message: 'status is updated', status: status });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+exports.updateStatus = async (req, res, next) => {
+  const newStatus = req.body.status;
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    user.status = newStatus;
+    await user.save();
+    res.status(201).json({ message: 'status is updated', status: status });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
