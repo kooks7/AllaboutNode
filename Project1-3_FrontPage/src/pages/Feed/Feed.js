66e41f866b6705f5181bfbef4e1f56a689c258bf
kooks7 <busanminjae@naver.com>
2020-04-01 15:52:44 +0900

    Add User Status Fun

--- Project1-3_FrontPage/src/pages/Feed/Feed.js
+++ Project1-3_FrontPage/src/pages/Feed/Feed.js
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -77,8 +81,18 @@ class Feed extends Component {
   };
 
   statusUpdateHandler = event => {
+    console.log('123123', event);
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status', {
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        status: this.state.status
+      })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
