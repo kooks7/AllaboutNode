8bee09a25fa05ed12745deb07a5b6731e6285272
kooks7 <busanminjae@naver.com>
2020-04-01 15:52:30 +0900

    Add User Status Fun

--- Project1-3/controllers/auth.js
+++ Project1-3/controllers/auth.js
@@ -75,3 +75,41 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => {
+  User.findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.updateStatus = (req, res, next) => {
+  const userId = req.userId;
+  const status = req.body.status;
+  User.findById(userId)
+    .then(user => {
+      user.status = status;
+      return user.save();
+    })
+    .then(result => {
+      console.log(result);
+      res.status(201).json({ message: 'status is updated', status: status });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- Project1-3/routes/auth.js
+++ Project1-3/routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require('express-validator/check');
 
 const User = require('../models/user');
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -33,5 +34,19 @@ router.put(
 );
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch(
+  '/status',
+  isAuth,
+  [
+    body('status')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.updateStatus
+);
 
 module.exports = router;
