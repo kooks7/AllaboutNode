2aca547d7e6285d3a5e513f5eb7f1d268d078bff
kooks7 <busanminjae@naver.com>
2020-04-06 18:58:31 +0900

    Add Login Function

--- Project1-3-GraphQL/graphql/resolvers.js
+++ Project1-3-GraphQL/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -35,5 +36,28 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login: async function({ email, password }) {
+    const user = await User.find({ email: email });
+    if (!user) {
+      const error = new Error('User not found');
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (isEqual) {
+      const error = new Error('Password is incorrect.');
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      'somesupersecret',
+      { expiresIn: '1h' }
+    );
+    return { token: token, userId: user._id.toString() };
   }
 };
