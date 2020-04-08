0b3eefd3dded98d0672fc4cff1085f780e41b941
kooks7 <busanminjae@naver.com>
2020-04-08 18:26:03 +0900

    Add Delete Post

--- Project1-3_FrontPage/src/pages/Feed/Feed.js
+++ Project1-3_FrontPage/src/pages/Feed/Feed.js
@@ -276,11 +276,20 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE',
+    const graphqlQuery = {
+      query: `
+      mutation{
+        deletePost(id: "${postId}")
+      }
+      `,
+    };
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
         Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
