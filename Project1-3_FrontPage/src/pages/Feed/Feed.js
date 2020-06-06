d087ea900f4010a70f7563651dcad605b5f4e0e0
kooks7 <busanminjae@naver.com>
2020-06-06 21:02:51 +0900

    Add Feed

--- Project1-3_FrontPage/src/pages/Feed/Feed.js
+++ Project1-3_FrontPage/src/pages/Feed/Feed.js
@@ -278,12 +278,14 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 2) {
               updatedPosts.pop();
             }
@@ -294,6 +296,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           };
         });
       })
